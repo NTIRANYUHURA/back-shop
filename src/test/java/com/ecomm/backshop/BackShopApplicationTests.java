30ff4b3e3103f57cba6198cabbfddabba28a2f04
NTIRANYUHURA <floribertnt@gmail.com>
2024-01-17 11:49:23 +0100

    link springboot to database

---
+++ src/test/java/com/ecomm/backshop/BackShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomm.backshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
